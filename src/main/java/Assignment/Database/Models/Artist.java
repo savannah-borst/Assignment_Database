df0e40f7414c5bff43edbddf602ccfc5dd42299b
savannah-borst <savannah.borst@gmail.com>
2022-02-20 14:05:00 +0100

    Adding model Artist

---
+++ src/main/java/Assignment/Database/Models/Artist.java
@@ -0,0 +1,11 @@
+package Assignment.Database.Models;
+
+public class Artist {
+    private int artistId;
+    private String name;
+
+    public Artist(int artistId,String name) {
+        this.artistId = artistId;
+        this.name= name;
+    }
+}

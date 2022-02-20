7710b8b7d44785d2f58c1e20b822aee260057878
Richie Schuurman <richie.schuurman@hotmail.com>
2022-02-20 16:09:26 +0100

    Added new repository for adding/updating customers Added Add() method Changed Customer model

--- src/main/java/Assignment/Database/DatabaseApplication.java
+++ src/main/java/Assignment/Database/DatabaseApplication.java
@@ -1,13 +1,22 @@
 package Assignment.Database;
 
+import Assignment.Database.DataAccess.CustomerAddAndUpdate;
+import Assignment.Database.Models.Customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseApplication.class, args);
-	}
+		CustomerAddAndUpdate customerAddAndUpdate = new CustomerAddAndUpdate();
 
+		Customer richie = new Customer(80, "Richie", "Schuurman", "Nederland",
+				"1161XC", "0612345678", "richie@hotmail.com");
+
+		customerAddAndUpdate.addCustomer(richie);
+	}
 }

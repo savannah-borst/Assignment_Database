217e5f9ebf732a0a1cba537504216ecfc8e6c74c
Richie Schuurman <richie.schuurman@hotmail.com>
2022-02-20 14:32:32 +0100

    Added models: - Album - Customer - Employee - Invoice - InvoiceLine

---
+++ src/main/java/Assignment/Database/Models/Invoice.java
@@ -0,0 +1,100 @@
+package Assignment.Database.Models;
+
+import java.math.BigDecimal;
+
+public class Invoice {
+    private int invoiceId;
+    private int customerId;
+    private String invoiceDate;
+    private String billingAddress;
+    private String billingCity;
+    private String billingState;
+    private String billingCountry;
+    private String billingPostalCode;
+    private BigDecimal total;
+
+    public Invoice(int invoiceId, int customerId, String invoiceDate, String billingAddress, String billingCity,
+                   String billingState, String billingCountry, String billingPostalCode, BigDecimal total) {
+        this.invoiceId = invoiceId;
+        this.customerId = customerId;
+        this.invoiceDate = invoiceDate;
+        this.billingAddress = billingAddress;
+        this.billingCity = billingCity;
+        this.billingState = billingState;
+        this.billingCountry = billingCountry;
+        this.billingPostalCode = billingPostalCode;
+        this.total = total;
+    }
+
+    public int getInvoiceId() {
+        return invoiceId;
+    }
+
+    public void setInvoiceId(int invoiceId) {
+        this.invoiceId = invoiceId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getInvoiceDate() {
+        return invoiceDate;
+    }
+
+    public void setInvoiceDate(String invoiceDate) {
+        this.invoiceDate = invoiceDate;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getBillingCity() {
+        return billingCity;
+    }
+
+    public void setBillingCity(String billingCity) {
+        this.billingCity = billingCity;
+    }
+
+    public String getBillingState() {
+        return billingState;
+    }
+
+    public void setBillingState(String billingState) {
+        this.billingState = billingState;
+    }
+
+    public String getBillingCountry() {
+        return billingCountry;
+    }
+
+    public void setBillingCountry(String billingCountry) {
+        this.billingCountry = billingCountry;
+    }
+
+    public String getBillingPostalCode() {
+        return billingPostalCode;
+    }
+
+    public void setBillingPostalCode(String billingPostalCode) {
+        this.billingPostalCode = billingPostalCode;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+}

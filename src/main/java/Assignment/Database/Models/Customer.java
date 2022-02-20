5c65e697cf3166a73c6587fd942af0319a0436a9
Richie Schuurman <richie.schuurman@hotmail.com>
2022-02-20 16:12:41 +0100

    Changed Customer model in master branch

--- src/main/java/Assignment/Database/Models/Customer.java
+++ src/main/java/Assignment/Database/Models/Customer.java
@@ -4,32 +4,19 @@ public class Customer {
     private int customerId;
     private String firstName;
     private String lastName;
-    private String company;
-    private String address;
-    private String city;
-    private String state;
     private String country;
     private String postalCode;
     private String phone;
-    private String fax;
     private String email;
-    private int supportRepId;
 
-    public Customer(int customerId, String firstName, String lastName, String company, String address, String city,
-                    String state, String country, String postalCode, String phone, String fax, String email, int supportRepId) {
+    public Customer(int customerId, String firstName, String lastName, String country, String postalCode, String phone, String email) {
         this.customerId = customerId;
         this.firstName = firstName;
         this.lastName = lastName;
-        this.company = company;
-        this.address = address;
-        this.city = city;
-        this.state = state;
         this.country = country;
         this.postalCode = postalCode;
         this.phone = phone;
-        this.fax = fax;
         this.email = email;
-        this.supportRepId = supportRepId;
     }
 
     public int getCustomerId() {
@@ -56,38 +43,6 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public String getCompany() {
-        return company;
-    }
-
-    public void setCompany(String company) {
-        this.company = company;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public String getCountry() {
         return country;
     }
@@ -112,27 +67,11 @@ public class Customer {
         this.phone = phone;
     }
 
-    public String getFax() {
-        return fax;
-    }
-
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public int getSupportRepId() {
-        return supportRepId;
-    }
-
-    public void setSupportRepId(int supportRepId) {
-        this.supportRepId = supportRepId;
     }
 }

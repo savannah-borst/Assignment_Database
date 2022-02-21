55dfc4e50363703adcec0a78aeebdc7892cd7d18
savannah-borst <savannah.borst@gmail.com>
2022-02-21 09:51:12 +0100

    Uniform exceptions to agreed way + added getCustomerByName method

--- src/main/java/Assignment/Database/DataAccess/CustomerRepository.java
+++ src/main/java/Assignment/Database/DataAccess/CustomerRepository.java
@@ -10,6 +10,7 @@ public class CustomerRepository {
     private String URL = ConnectionHelper.ConnectionURL;
     private Connection conn = null;
 
+    //Read all the customers in the database
     public ArrayList<Customer> getAllCustomers() {
         ArrayList<Customer> customers = new ArrayList<>();
 
@@ -27,8 +28,8 @@ public class CustomerRepository {
             //Process results
             while (resultSet.next()) {
                 customers.add(
-                        //int customerId, String firstName, String lastName, String company, String address, String city,
-                        //                    String state, String country, String postalCode, String phone, String fax, String email, int supportRepId
+
+                        //Make Query display: Id, first name, last name, country, postal code, phone number and email.
                         new Customer(
                                 resultSet.getInt("CustomerId"),
                                 resultSet.getString("FirstName"),
@@ -40,21 +41,24 @@ public class CustomerRepository {
                         ));
             }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("Please check your query");
+            System.out.println(exception.toString());
         } finally {
             try {
                 conn.close();
-            } catch (Exception ex) {
+            } catch (Exception exception) {
                 System.out.println("something went wrong while closing connection");
-                System.out.println(ex.toString());
+                System.out.println(exception.toString());
             }
         }
         return customers;
     }
 
+    //Read a specific customer from the database (by Id)
     public Customer getCustomerById(int customerId) {
         Customer customer = null;
+
         try {
             //Connect to database
             conn = DriverManager.getConnection(URL);
@@ -78,17 +82,64 @@ public class CustomerRepository {
                         resultSet.getString("Email")
                 );
             }
-            System.out.println("Select specific customer succesful");
+            System.out.println("Select specific customer successful");
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (Exception exception) {
+            System.out.println("Please check your query");
+            System.out.println(exception);
         } finally {
             try {
                 conn.close();
-            } catch (Exception e) {
-                System.out.println(e.toString());
+                System.out.println("Closed connection with success");
+            } catch (Exception exception) {
+                System.out.println("something went wrong while closing connection");
+                System.out.println(exception.toString());
             }
         }
         return customer;
         }
+
+        //Read a specific customer by name
+    public Customer getCustomerByName(String firstName) {
+        Customer customer = null;
+
+        try {
+            //Connect to Database
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to Chinook has been established.");
+
+            //Make query
+            PreparedStatement preparedStatement = conn.prepareStatement("SELECT CustomerId, FirstName, LastName, Country, PostalCode, Phone, Email FROM Customer WHERE FirstName LIKE ?");
+            preparedStatement.setString(1, firstName);
+
+            //Execute Statement
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                customer = new Customer(
+                        resultSet.getInt("CustomerId"),
+                        resultSet.getString("FirstName"),
+                        resultSet.getString("LastName"),
+                        resultSet.getString("Country"),
+                        resultSet.getString("PostalCode"),
+                        resultSet.getString("Phone"),
+                        resultSet.getString("Email")
+                );
+            }
+            System.out.println("Select specific customer successful");
+
+        } catch (Exception exception)  {
+            System.out.println("Please check your query");
+            System.out.println(exception);
+        } finally {
+            try {
+                conn.close();
+                System.out.println("Closed connection with success");
+            } catch (Exception exception) {
+                System.out.println("something went wrong while closing connection");
+                System.out.println(exception.toString());
+            }
+        }
+        return customer;
+    }
 }

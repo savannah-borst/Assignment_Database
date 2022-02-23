1edb10c529bbd4945cb4c2da3a822dc904e8625c
savannah-borst <savannah.borst@gmail.com>
2022-02-23 11:53:05 +0100

    added random to models for homepage

--- src/main/java/Assignment/Database/Controllers/CustomerThymeController.java
+++ src/main/java/Assignment/Database/Controllers/CustomerThymeController.java
@@ -1,15 +1,19 @@
 package Assignment.Database.Controllers;
 
+import Assignment.Database.DataAccess.RandomRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CustomerThymeController {
+    RandomRepository randomRepo = new RandomRepository();
 
     @GetMapping("/home")
     public String homePage(Model model){
-        model.addAttribute("greeting", "Hallo Richie & Savannah");
+        model.addAttribute("artists", randomRepo.randomArtists());
+        model.addAttribute("songs", randomRepo.randomSongs());
+        model.addAttribute("genres", randomRepo.randomGenres());
         return "home";
     }
 

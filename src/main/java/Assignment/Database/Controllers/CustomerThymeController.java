cfd0bdd3137b9bd51d79ba8bd47462fa534d1cec
savannah-borst <savannah.borst@gmail.com>
2022-02-23 11:01:09 +0100

    testing thymeleaf

--- src/main/java/Assignment/Database/Controllers/CustomerThymeController.java
+++ src/main/java/Assignment/Database/Controllers/CustomerThymeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CustomerThymeController {
 
-    @GetMapping()
+    @GetMapping("/home")
     public String homePage(Model model){
         model.addAttribute("greeting", "Hallo Richie & Savannah");
         return "home";

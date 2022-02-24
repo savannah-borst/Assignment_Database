cb232f2a0e7637d5e4ffd8287caf42e8ca87fd78
savannah-borst <savannah.borst@gmail.com>
2022-02-24 09:00:35 +0100

    handleSearch controller

--- src/main/java/Assignment/Database/Controllers/ThymeleafController.java
+++ src/main/java/Assignment/Database/Controllers/ThymeleafController.java
@@ -2,10 +2,15 @@ package Assignment.Database.Controllers;
 
 import Assignment.Database.DataAccess.RandomRepository;
 import Assignment.Database.DataAccess.SearchRepository;
+import Assignment.Database.Models.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class ThymeleafController {
@@ -24,14 +29,15 @@ public class ThymeleafController {
     @GetMapping("/search")
     public String searchPage(Model model) {
         model.addAttribute("headertext", "Welcome to the Chinook Music Database");
+        model.addAttribute("search", )
         return "search";
     }
 
     @PostMapping("/search")
-    public String handleSearch() {
-        boolean success = searchRepo.getSearch();
-
-        return search;
+    public String handleSearch(@ModelAttribute String searchQuery, BindingResult errors, Model model) {
+        ArrayList<Search> returnSearch = searchRepo.getSearch(searchQuery);
+        model.addAttribute("returnSearch", returnSearch);
+        return "search";
     }
 
 }

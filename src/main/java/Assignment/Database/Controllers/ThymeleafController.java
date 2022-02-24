7d533d086d9da97385322c30b660ff11188398e7
savannah-borst <savannah.borst@gmail.com>
2022-02-24 08:18:54 +0100

    Added  postmapping for the  search

--- src/main/java/Assignment/Database/Controllers/ThymeleafController.java
+++ src/main/java/Assignment/Database/Controllers/ThymeleafController.java
@@ -1,13 +1,16 @@
 package Assignment.Database.Controllers;
 
 import Assignment.Database.DataAccess.RandomRepository;
+import Assignment.Database.DataAccess.SearchRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ThymeleafController {
     RandomRepository randomRepo = new RandomRepository();
+    SearchRepository searchRepo = new SearchRepository();
 
     @GetMapping("/home")
     public String homePage(Model model){
@@ -22,6 +25,13 @@ public class ThymeleafController {
     public String searchPage(Model model) {
         model.addAttribute("headertext", "Welcome to the Chinook Music Database");
         return "search";
+    }
+
+    @PostMapping("/search")
+    public String handleSearch() {
+        boolean success = searchRepo.getSearch();
+
+        return search;
     }
 
 }

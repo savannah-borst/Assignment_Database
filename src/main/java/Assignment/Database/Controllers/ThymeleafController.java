cc0fde967202f742163742c27d6cfe6e36080c1c
savannah-borst <savannah.borst@gmail.com>
2022-02-24 14:49:16 +0100

    made the search function work! yaay

--- src/main/java/Assignment/Database/Controllers/ThymeleafController.java
+++ src/main/java/Assignment/Database/Controllers/ThymeleafController.java
@@ -3,6 +3,7 @@ package Assignment.Database.Controllers;
 import Assignment.Database.DataAccess.RandomRepository;
 import Assignment.Database.DataAccess.SearchRepository;
 import Assignment.Database.Models.Search;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,34 +18,22 @@ import java.util.ArrayList;
 public class ThymeleafController {
     RandomRepository randomRepo = new RandomRepository();
     SearchRepository searchRepo = new SearchRepository();
-    ArrayList<Search> returnSearch = new ArrayList<>();
+
+
 
     @GetMapping("/home")
     public String homePage(Model model){
         model.addAttribute("headertext", "Welcome to the Chinook Music Database");
-        model.addAttribute("randartist", randomRepo.randomArtists());
-        model.addAttribute("randsong", randomRepo.randomSongs());
-        model.addAttribute("randgenre", randomRepo.randomGenres());
+        model.addAttribute("artist", randomRepo.randomArtists());
+        model.addAttribute("song", randomRepo.randomSongs());
+        model.addAttribute("genre", randomRepo.randomGenres());
         return "home";
-    }
-    @PostMapping("/search")
-    public String handleSearch(@ModelAttribute String searchQuery, BindingResult errors, Model model) {
-        returnSearch = searchRepo.getSearch(searchQuery);
-        model.addAttribute("returnSearch", returnSearch);
-        return "search";
     }
 
     @GetMapping("/search")
-    public String searchPage(@RequestParam(name = "searchQuery", required = false) String searchQuery, Model model) {
-        model.addAttribute("headertext", "Welcome to the Chinook Music Database");
-        if (searchQuery != null && !searchQuery.equals("")) {
-            model.addAttribute("track", returnSearch);
-            model.addAttribute("artist", searchRepo.getSearch(searchQuery));
-            model.addAttribute("album", searchRepo.getSearch(searchQuery));
-            model.addAttribute("genre", searchRepo.getSearch(searchQuery));
-        } else {
-            model.addAttribute("fail", "Could not find track");
-        }
+    public String searchPage(@RequestParam String searchQuery, Model model) {
+        ArrayList<Search> searchResult = searchRepo.getSearch(searchQuery);
+        model.addAttribute("searchResults" , searchResult);
         return "search";
     }
 
